3323e52b76c101f7e77215cf9135db8ad36db499
PozitGo <PozitGo.pozitgo2005@gmail.com>
2023-04-29 12:44:12 +0300

    Добавьте файлы проекта.

---
+++ TCPClient/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+public class TCPClient
+{
+    private String server;
+    private Int32 port;
+
+    public TCPClient()
+    {
+        server = "127.0.0.1";
+        port = 13000;
+    }
+
+    public async Task StartAsync()
+    {
+        try
+        {
+            TcpClient client = new TcpClient(server, port);
+            Console.WriteLine("Подключено к серверу {0}:{1}", server, port);
+
+            NetworkStream stream = client.GetStream();
+
+            // Отправляем сообщение серверу
+            String message = "Привет от клиента";
+            byte[] msg = Encoding.UTF8.GetBytes(message);
+            await stream.WriteAsync(msg, 0, msg.Length);
+            Console.WriteLine("Отправлено сообщение: {0}", message);
+
+            // Получаем ответ от сервера
+            msg = new byte[256];
+            String response = String.Empty;
+            Int32 bytes = await stream.ReadAsync(msg, 0, msg.Length);
+            response = Encoding.UTF8.GetString(msg, 0, bytes);
+            Console.WriteLine("Получен ответ: {0}", response);
+
+            stream.Close();
+            client.Close();
+            Console.WriteLine("Соединение закрыто.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка подключения: {0}", ex.Message);
+        }
+    }
+}
+
+public class ClientProgram
+{
+    public static async Task Main()
+    {
+        TCPClient client = new TCPClient();
+        await client.StartAsync();
+    }
+}
---
+++ TCPServer/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+public class TCPServer
+{
+    private TcpListener server;
+    private Int32 port;
+    private IPAddress localAddr;
+
+    public TCPServer()
+    {
+        localAddr = IPAddress.Loopback;
+        port = 13000;
+        server = new TcpListener(localAddr, port);
+    }
+
+    public async Task StartAsync()
+    {
+        server.Start();
+        Console.WriteLine("Сервер запущен на {0}:{1}", localAddr, port);
+
+        while (true)
+        {
+            Console.WriteLine("Ожидание подключения клиента...");
+            TcpClient client = await server.AcceptTcpClientAsync();
+            Console.WriteLine("Клиент подключен!");
+
+            await Task.Factory.StartNew(async ()  => {
+               await HandleClientRequestAsync(client);
+            });
+        }
+    }
+
+    private async Task HandleClientRequestAsync(object obj)
+    {
+        TcpClient client = (TcpClient)obj;
+
+        Byte[] bytes = new Byte[256];
+        String data = null;
+        NetworkStream stream = client.GetStream();
+
+        int i;
+        while ((i = await stream.ReadAsync(bytes, 0, bytes.Length)) != 0)
+        {
+            data = Encoding.UTF8.GetString(bytes, 0, i);
+            Console.WriteLine("Получено сообщение: {0}", data);
+
+            // Отправляем сообщение обратно клиенту
+            byte[] buffer = Encoding.UTF8.GetBytes("Привет, от сервера");
+            await stream.WriteAsync(buffer, 0, buffer.Length);
+            Console.WriteLine("Отправлено сообщение: {0}", data);
+        }
+
+        client.Close();
+        Console.WriteLine("Соединение закрыто.\n");
+    }
+}
+
+public class ServerProgram
+{
+    public static async Task Main()
+    {
+        TCPServer server = new TCPServer();
+        await server.StartAsync();
+    }
+}
